59c26223b1d865051571abe714156fa9800aa1c3
TUKYO <tukyowave@gmail.com>
2024-11-24 22:54:25 -0800

    fix

--- scripts/teleworker.py
+++ scripts/teleworker.py
@@ -1,18 +1,20 @@
 import sys
-from telethon.sync import TelegramClient
+from telethon import TelegramClient
 from telethon.tl.functions.channels import GetParticipantsRequest
 from telethon.tl.types import ChannelParticipantsSearch
-
 from scripts import config
 
 if len(sys.argv) != 2:
-    print("Usage: python telethon_worker.py <chat_id>")
+    print("Usage: python teleworker.py <chat_id>")
     sys.exit(1)
 
 chat_id = sys.argv[1]
 
 async def log_deleted(chat_id):
-    async with TelegramClient("bot", config.API_ID, config.API_HASH).start(bot_token=config.TELEGRAM_TOKEN) as client:
+    async with TelegramClient("bot", config.API_ID, config.API_HASH) as client:
+        # Start the client explicitly
+        await client.start(bot_token=config.TELEGRAM_TOKEN)
+
         group_entity = await client.get_entity(int(chat_id))
         offset = 0
         limit = 100
